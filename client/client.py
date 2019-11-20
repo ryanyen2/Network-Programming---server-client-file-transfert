d1da8be1a3505555cea6e485b5ca663ed1c23c7a
ryan yen <45389924+ryanyen2@users.noreply.github.com>
2019-11-20 21:34:53 +0800

    Update client.py

--- client/client.py
+++ client/client.py
@@ -5,31 +5,31 @@ s = socket.socket()  # Create a socket object
 port = 12345  # Reserve a port for your service.
 timeout = time.time() + 10
 
-host = input('PPlease Input a IP Address: ')
-s.connect((host, port))
+host = input('Please Input a IP Address: ')
+s.connect((host, port))     # set up the connection
 print(u'the server %s:%s has connected.' % (host, port))
 
 file_name = input('Input the file name to be requested from the server: ')
-s.sendall(file_name.encode())
+s.sendall(file_name.encode())   # request the file from the server side
 
-with open(file_name, 'wb') as f:
+with open(file_name, 'wb') as f:    # Open a file i client side
     print('file opened')
     while True:
-        data = s.recv(1024)
+        data = s.recv(1024)     # receive data in bytes form
         if not data:
             f.close()
             print('file close()')
             break
         print('Send Status: Success')
         print('Open File Status: success')
-        x = data.decode("utf-8")
-        x.rstrip('\x00')
+        x = data.decode("utf-8")        # decode the byte data to string
+        x.rstrip('\x00')        # get rid of the '\x00' string
         print('Received data: ', x)
         # write data to a file
-        f.write(data)
-        if time.time() < timeout:
+        f.write(data)       # write the data to file
+        if time.time() < timeout:       # set the time to break the loop, prevent the timeout situation
             break
 
 print('Receive File ' + file_name + ' From the Server Successful!')
-s.close()
+s.close()       # close the connection
 print('connection closed')
